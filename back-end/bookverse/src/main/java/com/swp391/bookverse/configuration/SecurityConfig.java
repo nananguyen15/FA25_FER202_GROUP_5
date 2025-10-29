c135239381b445516cfa5dfc93d1fc2a3565e736
Nguyen Hoang Dat <datnhce180797@fpt.edu.vn>
2025-10-29 13:10:30 +0700

    config CORS for calling API from frontend

--- back-end/bookverse/src/main/java/com/swp391/bookverse/configuration/SecurityConfig.java
+++ back-end/bookverse/src/main/java/com/swp391/bookverse/configuration/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -63,6 +64,7 @@ public class SecurityConfig {
      */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.cors(Customizer.withDefaults());
 
         // Define authorization rules for different endpoints and HTTP methods based on user roles and scopes in JWT
         httpSecurity.authorizeHttpRequests(request ->
