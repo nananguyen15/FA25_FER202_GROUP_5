7b80d4ecae2ec5b4c019e8daee851ef9a517b2f5
Nguyen Hoang Dat <datnhce180797@fpt.edu.vn>
2025-11-06 11:09:03 +0700

    Backend/base (#23)
    * update book's filering api

--- back-end/bookverse/src/main/java/com/swp391/bookverse/controller/BookController.java
+++ back-end/bookverse/src/main/java/com/swp391/bookverse/controller/BookController.java
@@ -39,6 +39,41 @@ public class BookController {
         return response;
     }
 
+    @GetMapping("/active/sort-by-newest")
+    public APIResponse<List<BookResponse>> getActiveBooksSortedByNewest() {
+        APIResponse<List<BookResponse>> response;
+        response = bookService.getActiveBooksSortedByNewest();
+        return response;
+    }
+
+    @GetMapping("/active/sort-by-oldest")
+    public APIResponse<List<BookResponse>> getActiveBooksSortedByOldest() {
+        APIResponse<List<BookResponse>> response;
+        response = bookService.getActiveBooksSortedByOldest();
+        return response;
+    }
+
+    @GetMapping("/active/sort-by-price-asc")
+    public APIResponse<List<BookResponse>> getActiveBooksSortedByPriceAsc() {
+        APIResponse<List<BookResponse>> response;
+        response = bookService.getActiveBooksSortedByPriceAsc();
+        return response;
+    }
+
+    @GetMapping("/active/sort-by-price-desc")
+    public APIResponse<List<BookResponse>> getActiveBooksSortedByPriceDesc() {
+        APIResponse<List<BookResponse>> response;
+        response = bookService.getActiveBooksSortedByPriceDesc();
+        return response;
+    }
+
+    @GetMapping("/active/sort-by-title")
+    public APIResponse<List<BookResponse>> getActiveBooksSortedByTitleAsc() {
+        APIResponse<List<BookResponse>> response;
+        response = bookService.getActiveBooksSortedByTitleAsc();
+        return response;
+    }
+
     @GetMapping("/{bookId}")
     public BookResponse getBook(@PathVariable("bookId") String bookId) {
         return bookService.getBookById(bookId);
@@ -80,12 +115,13 @@ public class BookController {
         return response;
     }
 
-//    @GetMapping("/active/search")
-//    public APIResponse<List<BookResponse>> searchActiveBooks(@RequestParam("keyword") String keyword) {
-//        APIResponse<List<BookResponse>> response;
-//        response = bookService.searchActiveBooks(keyword);
-//        return response;
-//    }
+
+    @GetMapping("/active/search/{title}")
+    public APIResponse<List<BookResponse>> searchActiveBooksByTitle(@PathVariable("title") String title) {
+        APIResponse<List<BookResponse>> response;
+        response = bookService.searchActiveBooksByTitle(title);
+        return response;
+    }
 
 
 //    @GetMapping("/active/top-selling")
